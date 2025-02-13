1011dd10ab4e3f26650559995411cb83ffe3c2a3
dkuptzov <101925838+dkuptzov@users.noreply.github.com>
2025-02-13 13:23:16 +0300

    Update dmanager_module.c
    1

--- dmanager_module.c
+++ dmanager_module.c
@@ -13,32 +13,18 @@ struct door {
 };
 
 void initialize_doors(struct door* doors);
+void change_doors(struct door* doors);
+void out(struct door* doors);
 
 int main()
 {
-	struct door doors[DOORS_COUNT];
-	initialize_doors(doors);
-
-	for (int i = 0; i < DOORS_COUNT - 1; i++)
-	{
-		for (int j = 0; j < DOORS_COUNT - 1 - i; j++)
-		{
-			if (doors[j].id > doors[j + 1].id)
-			{
-				struct door temp = doors[j];
-				doors[j] = doors[j + 1];
-				doors[j + 1] = temp;
-			}
-		}
-	}
-
-	for (int i = 0; i < DOORS_COUNT; i++)
-		doors[i].status = 1;
-
-	return 0;
+        struct door doors[DOORS_COUNT];
+        initialize_doors(doors);
+        change_doors(doors);
+        out(doors);
 }
 
-// Doors initialization function
+/ Doors initialization function
 // ATTENTION!!!
 // DO NOT CHANGE!
 void initialize_doors(struct door* doors)
@@ -52,3 +38,31 @@ void initialize_doors(struct door* doors)
         doors[i].status = rand() % 2;
     }
 }
+
+void change_doors(struct door* doors)
+{
+        for (int i = 0; i < DOORS_COUNT - 1; i++)
+        {
+                for (int j = 0; j < DOORS_COUNT - 1 - i; j++)
+                {
+                        if (doors[j].id > doors[j + 1].id)
+                        {
+                                struct door temp = doors[j];
+                                doors[j] = doors[j + 1];
+                                doors[j + 1] = temp;
+                        }
+                }
+        }
+
+        for (int i = 0; i < DOORS_COUNT; i++)
+                doors[i].status = 0;
+}
+
+void out(struct door* doors)
+{
+        for (int i = 0 ; i < DOORS_COUNT; i++)
+        {
+                printf("%d, %d\n", doors[i].id, doors[i].status);
+        }
+}
+
